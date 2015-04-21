3fc977b27e32259ecb535dc32b6c8cfab89cc547
David Maple <d@davemaple.com>
2015-04-21 12:11:06 -0400

    monit dependency

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,7 @@ maintainer  "David Maple"
 license     "Apache 2.0"
 version     "1.0.0"
 
-depends 'opsworks_initial_setup'
-depends 'opsworks_agent_monit'
+depends 'monit'
 depends 'jq'
 depends 'java8'
 depends 'zip'
