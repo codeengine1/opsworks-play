ab8fd0a00cad079b20767bda72917deb0d9ac408
David Maple <d@davemaple.com>
2015-04-21 12:38:42 -0400

    removes notify for now

--- recipes/default.rb
+++ recipes/default.rb
@@ -89,7 +89,6 @@ template "#{node[:monit][:conf_dir]}/play.monitrc" do
     cookbook 'play'
     mode '0600'
     source "monitrc.erb"
-    notifies :restart, "service[monit]"
 end
 
 include_recipe "play::service"
